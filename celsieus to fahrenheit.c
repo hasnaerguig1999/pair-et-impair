4102bc6daa5f7f0ebe19775efd1b16252712e948
hasnaerguig1999 <112939311+hasnaerguig1999@users.noreply.github.com>
2022-09-07 15:05:45 +0100

    Add files via upload

---
+++ celsieus to fahrenheit.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main() {
+	float c,f;
+	printf("entrer la dg\n");
+	scanf("%f",&c);
+	f=(c*9/5)+32;
+	printf("la dg est %f %f",c,f);
+	return 0;
+}
