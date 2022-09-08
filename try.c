0273ed12bf86da50581b76540bfd8e2d42c094cd
hasnaerguig1999 <112939311+hasnaerguig1999@users.noreply.github.com>
2022-09-08 14:04:58 +0100

    Add files via upload

---
+++ try.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+ int main(){
+
+	
+    int i,n;
+     int s=0;
+   
+    for(i=0;i<=4;i++)
+    {
+    	 printf("entrer le nombre des entier\n");
+    scanf("%d",&n);
+    	
+    s=s+n;	
+}
+	printf("la somme est %d",s);
+
+}
