97ba08c8213fb5cd3c5825fb1cfad05e9cea58e8
hasnaerguig1999 <112939311+hasnaerguig1999@users.noreply.github.com>
2022-09-13 11:27:50 +0100

    Add files via upload

---
+++ tableau des entien tout les elementzero.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	int T[4];
+	int i;
+	for(i=0;i<4;i++)
+	T[i]=0;
+	for(i=0;i<4;i++)
+	printf("T[%d]= %d\n",i,T[i]);
+	return 0;
+}
